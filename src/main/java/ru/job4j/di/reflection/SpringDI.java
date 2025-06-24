3a0fb03f91b3656be580f254f4bd35038fc2e9ea
Rustem Galiullin <zux.grr@gmail.com>
2025-06-24 14:24:42 +0500

    1. Spring DI. Запуск [#2091]

---
+++ src/main/java/ru/job4j/di/reflection/SpringDI.java
@@ -0,0 +1,18 @@
+package ru.job4j.di.reflection;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringDI {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Store.class);
+        context.register(ConsoleInput.class);
+        context.register(StartUI.class);
+        context.refresh();
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.addFromInput("What's your name?");
+        ui.print();
+    }
+}
